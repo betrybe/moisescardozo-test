d2cdad215f448e5981f0538e28bd2e3ce40733f5
Moises <mcpfla@gmail.com>
2021-09-03 19:13:47 -0300

    feat(app.js index.js routes.js): adicionando a configuração de rotas

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return <Link to="/wallet">Login</Link>;
   }
 }
 
